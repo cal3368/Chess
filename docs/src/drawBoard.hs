de39588da2e187785a6c5f1622e0b92eeaf139d6
jc9627 <jc9627@g.rit.edu>
2023-11-13 15:00:09 -0500

    drawboard made

--- docs/src/drawBoard.hs
+++ docs/src/drawBoard.hs
@@ -2,7 +2,7 @@ module DrawBoard
   ( Color (..),
     Type (..),
     Square,
-    Piece,
+    -- Piece,
     Location,
     Board,
     new,
@@ -10,11 +10,12 @@ module DrawBoard
     drawHorizontal,
     drawVertical,
     draw,
-    getStringCoor,
+    chessPiece,
   )
 where
 
 import Data.Map.Strict qualified as Map
+import System.IO
 
 data Color = White | Black
   deriving (Eq, Enum, Bounded, Read, Show)
@@ -22,16 +23,13 @@ data Color = White | Black
 data Type = Pawn | Rook | Knight | Bishop | Queen | King
   deriving (Eq, Enum, Bounded, Read, Show)
 
-data Square = Piece | Empty
-  deriving (Eq, Enum, Bounded, Read, Show)
-
-data Piece = Color :@: Type
+data Square = Piece Color Type | Empty
   deriving (Eq, Read, Show)
 
-type Location = (Char, Int)
+-- data Piece = Color :@: Type
+--   deriving (Eq, Read, Show)
 
--- data Player = White | Black
---   deriving (Eq, Ord, Read, Show)
+type Location = (Char, Int)
 
 data Board where
   Board :: (Map.Map (Int, Int) Square) -> Board
@@ -40,9 +38,42 @@ data Board where
 new :: Board
 new = Board (Map.fromList (newHelper 0 0))
 
+createPiecesRow :: Color -> [((Int, Int), Square)]
+createPiecesRow White =
+  [ ((0, 0), Piece White Rook),
+    ((0, 1), Piece White Knight),
+    ((0, 2), Piece White Bishop),
+    ((0, 3), Piece White Queen),
+    ((0, 4), Piece White King),
+    ((0, 5), Piece White Bishop),
+    ((0, 6), Piece White Knight),
+    ((0, 7), Piece White Rook)
+  ]
+createPiecesRow Black =
+  [ ((7, 0), Piece Black Rook),
+    ((7, 1), Piece Black Knight),
+    ((7, 2), Piece Black Bishop),
+    ((7, 3), Piece Black Queen),
+    ((7, 4), Piece Black King),
+    ((7, 5), Piece Black Bishop),
+    ((7, 6), Piece Black Knight),
+    ((7, 7), Piece Black Rook)
+  ]
+
+createWhitePawns :: Int -> [((Int, Int), Square)]
+createWhitePawns 7 = [((1, 7), Piece White Pawn)]
+createWhitePawns col = ((1, col), Piece White Pawn) : createWhitePawns (succ col)
+
+createBlackPawns :: Int -> [((Int, Int), Square)]
+createBlackPawns 7 = [((1, 7), Piece Black Pawn)]
+createBlackPawns col = ((6, col), Piece Black Pawn) : createBlackPawns (succ col)
+
 newHelper :: Int -> Int -> [((Int, Int), Square)]
 newHelper curm curn
-  | curm + 1 == 8 && curn + 1 == 8 = [((curm, curn), Empty)]
+  | curm == 0 = createPiecesRow White ++ newHelper (curm + 1) 0
+  | curm == 1 = createWhitePawns 0 ++ newHelper (curm + 1) 0
+  | curm == 6 = createBlackPawns 0 ++ newHelper (curm + 1) 0
+  | curm + 1 == 8 = createPiecesRow Black
   | curn + 1 == 8 = ((curm, curn), Empty) : newHelper (curm + 1) 0
   | otherwise = ((curm, curn), Empty) : newHelper curm (curn + 1)
 
@@ -55,15 +86,31 @@ draw cur_row (Board coor)
   | cur_row == 1 = drawVertical cur_row 7 (Map.toList coor) ++ "A B C D E F G H"
   | otherwise = drawVertical cur_row 7 (Map.toList coor) ++ drawHorizontal 7 ++ draw (pred cur_row) (Board (Map.drop 8 coor))
 
--- Need to change case for piece on square
-getStringCoor :: ((Int, Int), Square) -> String
-getStringCoor ((_, _), Empty) = " "
-getStringCoor ((_, _), piece) = " "
+chessPiece :: Square -> String
+-- chessPiece Empty = " "
+-- chessPiece (Piece White Pawn) = "\x2659"
+-- chessPiece (Piece White Rook) = "\x2656"
+-- chessPiece (Piece White Knight) = "\x2658"
+-- chessPiece (Piece White Bishop) = "\x2657"
+-- chessPiece (Piece White Queen) = "\x2655"
+-- chessPiece (Piece White King) = "\x2654"
+-- chessPiece (Piece Black Pawn) = "\x265F"
+-- chessPiece (Piece Black Rook) = "\x265C"
+-- chessPiece (Piece Black Knight) = "\x265E"
+-- chessPiece (Piece Black Bishop) = "\x265D"
+-- chessPiece (Piece Black Queen) = "\x265B"
+-- chessPiece (Piece Black King) = "\x265A"
+chessPiece _ = " "
 
-test = draw 8 new
+test = new
 
 drawVertical :: Int -> Int -> [((Int, Int), Square)] -> String
-drawVertical _ 0 (x : xs) = getStringCoor x ++ "\n"
+drawVertical _ 0 (x : xs) = chessPiece (snd x) ++ "\n"
 drawVertical cur_row num (x : xs)
-  | num == 7 = show cur_row ++ " " ++ getStringCoor x ++ "|" ++ drawVertical cur_row (num - 1) xs
-  | otherwise = getStringCoor x ++ "|" ++ drawVertical cur_row (num - 1) xs
+  | num == 7 = show cur_row ++ " " ++ chessPiece (snd x) ++ "|" ++ drawVertical cur_row (num - 1) xs
+  | otherwise = chessPiece (snd x) ++ "|" ++ drawVertical cur_row (num - 1) xs
+
+main :: IO ()
+main = do
+  hSetEncoding stdout utf8
+  putStrLn (chessPiece (Piece White Pawn))
