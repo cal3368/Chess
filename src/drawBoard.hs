1e16a0cf28dd2e558f6a801dc5122e5ea2cd9a0b
cal3368 <cal3368@rit.edu>
2023-12-18 21:21:15 -0500

    removed test board

--- src/drawBoard.hs
+++ src/drawBoard.hs
@@ -60,36 +60,3 @@ getRowSquare 0 _ _ = []
 getRowSquare c n board = case Map.lookup (chr (96 + c), n) board of
   Nothing -> getRowSquare (pred c) n board ++ [Nothing]
   Just i -> getRowSquare (pred c) n board ++ [Just i]
-
-test :: Board
-test =
-  Map.fromList
-    [ (('a', 1), Piece White Rook True),
-      (('b', 1), Piece White Knight True),
-      (('c', 1), Piece White Bishop True),
-      (('f', 3), Piece White Queen True),
-      (('g', 1), Piece White King False),
-      (('f', 7), Piece White Bishop True),
-      (('f', 1), Piece White Rook False),
-      (('a', 8), Piece Black Rook True),
-      (('e', 5), Piece Black Knight True),
-      (('c', 8), Piece Black Bishop True),
-      (('d', 8), Piece Black Queen True),
-      (('f', 8), Piece Black King False),
-      (('d', 4), Piece Black Bishop True),
-      (('f', 6), Piece Black Knight True),
-      (('h', 8), Piece Black Rook True),
-      (('a', 7), Piece Black Pawn True),
-      (('b', 7), Piece Black Pawn True),
-      (('c', 7), Piece Black Pawn True),
-      (('d', 7), Piece Black Pawn True),
-      (('g', 7), Piece Black Pawn True),
-      (('h', 7), Piece Black Pawn True),
-      (('a', 2), Piece White Pawn True),
-      (('b', 2), Piece White Pawn True),
-      (('c', 2), Piece White Pawn True),
-      (('e', 4), Piece White Pawn True),
-      (('f', 2), Piece White Pawn True),
-      (('g', 2), Piece White Pawn True),
-      (('h', 2), Piece Black Rook True)
-    ]
